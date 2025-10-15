eec1b10925c5b42e89d2c2ed9fc227f6227d011d
Osvaldo Q. <quintanalosvaldo@gmail.com>
2025-10-15 14:54:56 -0400

    Implemented Basic Audio Functionality
    - Item makes a placeholder sound whenever you interact with it
    - Added subfolders in the Audio folder
    - Added a mixer track for Music, SFX, and Ambience

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,8 +6,17 @@ public class Item : Interactable
 {
     // This has not been finished yet
     // This is only here so I can test the PlayerInteraction script
+
+    public AudioClip testSound;
+
     public override void Interact(GameObject player, Item activeItem)
     {
+        if (testSound != null)
+        {
+            AudioManager audioManager = AudioManager.instance;
+            audioManager.PlaySoundFX(testSound, transform.position, 1.0f);
+        }
+
         Debug.Log("Item detected!");
     }
 }
