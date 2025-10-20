018e1cc556c320b2073006d765382e283f09cc9d
Raizel Quintana <raizelq84@gmail.com>
2025-10-20 16:40:45 -0400

    Updating ItemIDs

--- Assets/Scripts/ItemIDs.cs
+++ Assets/Scripts/ItemIDs.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemIDs : MonoBehaviour
+public class ItemIDs : Item
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum ItemId
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Suggested Items for now
+        BATTERY,
+        FLASHLIGHT,
+        KEYCARD,
+        MEDKIT
     }
 }
