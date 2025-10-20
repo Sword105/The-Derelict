f06c796ba8da3542b8ad5f47c3b8d29b4d1d7faf
Theolorex <30479282+Theolorex@users.noreply.github.com>
2025-10-20 02:11:52 -0400

    ssaving DO PARABOLAS I LOVE MATH

--- Assets/Scripts/VentObject.cs
+++ Assets/Scripts/VentObject.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class VentObject : Interactable
 {
     [SerializeField] private GameObject[] connectedVents; //array in case we want multiple connections somehow
-    [SerializeField] private Vector3 ventCenter;
+    [SerializeField] private Vector3 ventCenter; //vent center, put down in inspector for ease of testing
+    [SerializeField] private float ventRadius; //circle around vent, used for determining curve
+    private Vector3 playerDestination;
     void Start()
     {
         
@@ -28,14 +30,32 @@ public class VentObject : Interactable
 
         //player is immortal while venting
     }
+    
+    private Vector3 findPlayerDestination(Camera playerCamera)
+    {
+
+        Vector3 playerMirror = new Vector3( //FIRST: find the mirror coord
+            2 * ventCenter.x - playerCamera.transform.position.x, //mirrored coord in relation to ventCenter
+            transform.position.y, //same height
+            transform.position.z //same depth
+        );
+
+
+        return playerDestination;
+    }
 
     private void playerVisual(GameObject player)
     {
-        Camera PlayerCamera = player.GetComponent<Camera>(); //get players camera
+        Camera playerCamera = player.GetComponent<Camera>(); //get players camera //EDIT: do this with serialize field instead?
         
+        playerDestination = findPlayerDestination(playerCamera);
+        
+        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, ventCenter, 0.5f);
         
         //camera overcompensates if at a very parallel angle, at a perpendicular angle the camera swiftly reaches its destination 
         //vent opens
         //then slowly enters vent straight ahead, two different lerps
+        
+        //inside vent animation
     }
 }
