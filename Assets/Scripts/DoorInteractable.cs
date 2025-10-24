c24101fd9ee575124c85354ab28ef51e35551fa2
Gabriela Lopez <gabrielalopez2026@gmail.com>
2025-10-24 18:28:56 -0400

    please door stay open TT

---
+++ Assets/Scripts/DoorInteractable.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DoorInteractable : Interactable
+{
+    [SerializeField] public GameObject doorObject;
+    [SerializeField] public bool doorOpen = false;
+
+    // Placeholder method for door interaction when keys are added   
+    public override void Interact(GameObject player, Item activeItem)
+    {
+        Debug.Log(player.name + " is interacting with door " + gameObject.name);
+
+        if (doorOpen)
+        {
+            Debug.Log("The door opens.");
+            doorOpen = true;
+            doorObject.GetComponentInChildren<Animator>().Play("door_2_open");
+        }
+    
+    }
+
+}
