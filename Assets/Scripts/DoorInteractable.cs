d6e69a380ac817e6ee582609d86c331011b32a6e
Gabriela Lopez <gabrielalopez2026@gmail.com>
2025-10-27 00:53:15 -0400

    i fix door

--- Assets/Scripts/DoorInteractable.cs
+++ Assets/Scripts/DoorInteractable.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 public class DoorInteractable : Interactable
 {
     [SerializeField] public GameObject doorObject;
-    [SerializeField] public bool doorOpen = false;
+    [SerializeField] public bool IsOpen = false; 
     public override void Interact(GameObject player, Item activeItem)
     {
         Debug.Log(player.name + " is interacting with door " + gameObject.name);
 
-        if (doorOpen)
-        {
-            Debug.Log("The door opens.");
-            // doorOpen = true;
-            doorObject.GetComponentInChildren<Animator>().Play("door_2_open", 0, 0f);
-        }
-    
+        IsOpen = !IsOpen;
+        GetComponentInChildren<Animator>().SetBool("IsOpen", IsOpen);
+
+        // if (!isClosed)
+        // {
+        //     Debug.Log("The door opens.");
+        //     isClosed = false;
+        //     doorObject.GetComponentInChildren<Animator>().Play("door_2_open", 0, 0f);
+        // }
+        // else
+        // {
+        //     Debug.Log("The door closes.");
+        //     isClosed = true;
+        //     doorObject.GetComponentInChildren<Animator>().Play("door_2_close", 0, 0f);
+        // }   
+
     }
 
 }
